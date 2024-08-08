abf3b10f67467a74e93b43563427b8bf6aca5379
rihem1999 <rihem.farjallah1999@gmail.com>
2024-08-08 20:37:03 +0100

    design updated

--- lib/views/live_game_screen.dart
+++ lib/views/live_game_screen.dart
@@ -43,7 +43,9 @@ class _LiveGameScreenState extends State<LiveGameScreen> {
   }
 
   void _addTag() {
-    if (_selectedTeam != null && _tagDescription != null && _tagDescription!.isNotEmpty) {
+    if (_selectedTeam != null &&
+        _tagDescription != null &&
+        _tagDescription!.isNotEmpty) {
       setState(() {
         _tags.add({
           'team': _selectedTeam,
@@ -56,7 +58,10 @@ class _LiveGameScreenState extends State<LiveGameScreen> {
       Navigator.of(context).pop();
     } else {
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text('Please select a team and enter a description'),
+        content: Text(
+          'Select a team and enter a description',
+          style: TextStyle(fontFamily: 'Montserrat'),
+        ),
       ));
     }
   }
@@ -96,63 +101,76 @@ class _LiveGameScreenState extends State<LiveGameScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+
     return Scaffold(
       backgroundColor: const Color(0xFF171A25),
       appBar: AppBar(
         backgroundColor: const Color(0xFF171A25),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () =>  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                                builder: (context) => HomeScreen(),
-                              )),
+          onPressed: () =>
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          )),
         ),
         title: Row(
           children: [
             Container(
-              width: 30,
-              height: 30,
+              width: screenWidth * 0.1, // Adjust the width as a percentage of screen width
+              height: screenWidth * 0.1, // Maintain aspect ratio
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(5.0),
+                borderRadius: BorderRadius.circular(6.0),
               ),
               child: Image.asset(
-                widget.game.competitionPicture, // replace with your game logo asset path
+                widget.game
+                    .competitionPicture, // replace with your game logo asset path
                 fit: BoxFit.cover,
               ),
             ),
             const SizedBox(width: 8),
-            Text(
-              widget.game.competition,
-              style: const TextStyle(color: Colors.white),
+            Expanded(
+              child: Text(
+                widget.game.competition,
+                style: const TextStyle(color: Colors.white),
+                overflow: TextOverflow.ellipsis, // Handle overflow with ellipsis
+              ),
             ),
           ],
         ),
         elevation: 0,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(mediaQuery.size.width * 0.04), // Responsive padding
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.symmetric(vertical: mediaQuery.size.width * 0.02), // Responsive padding
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   _buildTeamLogo(widget.game.club1Picture, widget.game.clubName1),
                   const Text(
                     '-',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
                   ),
                   _buildTeamLogo(widget.game.club2Picture, widget.game.clubName2),
                 ],
               ),
             ),
             Center(
-              
-                child: Text(widget.game.startTime, style: const  TextStyle(color: Colors.white),),
+              child: Text(
+                widget.game.startTime,
+                style: const TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
               ),
-             const  SizedBox( height: 10),
+            ),
+            const SizedBox(height: 10),
             Center(
               child: ElevatedButton(
                 onPressed: _isGameStarted ? null : _startGame,
@@ -162,17 +180,26 @@ class _LiveGameScreenState extends State<LiveGameScreen> {
                     fontSize: 16.0,
                     color: Colors.white,
                   ),
-                  minimumSize: const Size(0, 50),
+                  minimumSize: Size(screenWidth * 0.5, 50), // Responsive button size
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(6.0),
                   ),
-                  disabledBackgroundColor: const Color.fromRGBO(116, 229, 144, 1.0),
+                  disabledBackgroundColor:
+                      const Color.fromRGBO(116, 229, 144, 1.0),
                   disabledForegroundColor: Colors.white,
                 ),
-                child: Text(_isGameStarted ? 'Game Started' : 'Start Game'),
+                child: Text(
+                  _isGameStarted ? 'Game Started' : 'Start Game',
+                  style: const TextStyle(fontFamily: 'Montserrat'),
+                ),
               ),
             ),
-            if (_isGameStarted) Center(child: TimerDisplay(elapsedSeconds: _elapsedSeconds, textColor: Colors.white)),
+            if (_isGameStarted)
+              Center(
+                  child: TimerDisplay(
+                      elapsedSeconds: _elapsedSeconds,
+                      textColor: Colors.white,
+                  )),
             if (_isGameStarted) ...[
               Center(
                 child: ElevatedButton(
@@ -183,28 +210,34 @@ class _LiveGameScreenState extends State<LiveGameScreen> {
                       fontSize: 16.0,
                       color: Colors.white,
                     ),
-                    minimumSize: const Size(0, 50),
+                    minimumSize: Size(screenWidth * 0.5, 50), // Responsive button size
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(6.0),
                     ),
                   ),
-                  child: const Text('Add Tag'),
+                  child: const Text(
+                    'Add Tag',
+                    style: TextStyle(fontFamily: 'Montserrat'),
+                  ),
                 ),
               ),
               Expanded(
                 child: ListView.separated(
                   itemCount: _tags.length,
-                  separatorBuilder: (context, index) => const Divider(color: Colors.white),
+                  separatorBuilder: (context, index) =>
+                      const Divider(color: Colors.white),
                   itemBuilder: (context, index) {
                     final tag = _tags[index];
                     return ListTile(
                       title: Text(
                         '${tag['team']} - ${tag['description']}',
-                        style: const TextStyle(color: Colors.white),
+                        style: const TextStyle(
+                            color: Colors.white, fontFamily: 'Montserrat'),
                       ),
                       subtitle: Text(
                         'Time: ${tag['time']}',
-                        style: const TextStyle(color: Colors.white),
+                        style: const TextStyle(
+                            color: Colors.white, fontFamily: 'Montserrat'),
                       ),
                     );
                   },
@@ -221,17 +254,19 @@ class _LiveGameScreenState extends State<LiveGameScreen> {
     return Column(
       children: [
         Container(
+          width: MediaQuery.of(context).size.width * 0.2, // Responsive size
+          height: MediaQuery.of(context).size.width * 0.2, // Maintain aspect ratio
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(8.0),
           ),
           padding: const EdgeInsets.all(8.0),
-          child: Image.asset(assetPath, width: 50, height: 50),
+          child: Image.asset(assetPath, fit: BoxFit.cover),
         ),
         const SizedBox(height: 8),
         Text(
           teamName,
-          style: const TextStyle(color: Colors.white),
+          style: const TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
         ),
       ],
     );
