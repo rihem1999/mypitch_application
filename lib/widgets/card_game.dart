bb0647ed65733906a8da25813d4790cbda0122dd
rihem1999 <rihem.farjallah1999@gmail.com>
2024-08-08 21:51:13 +0100

    style updated

--- lib/widgets/card_game.dart
+++ lib/widgets/card_game.dart
@@ -9,7 +9,7 @@ class GameCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Container(
       color: const Color(0xFF31343F), // Set the card background color here
       margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       child: Padding(
@@ -17,84 +17,95 @@ class GameCard extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              children: [
-                // Club 1 Image with Name
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset(
-                        game.club1Picture,
-                        width: 30,
-                        height: 30,
-                        fit: BoxFit.cover,
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Club 1 Image with Name
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(6.0),
+                        child: Image.asset(
+                          game.club1Picture,
+                          width: 40, // Adjust width as needed
+                          height: 40, // Adjust height as needed
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 12.0),
-                    Text(
-                      game.clubName1,
-                      style: const TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.white,
+                      const SizedBox(width: 12.0),
+                      Text(
+                        game.clubName1,
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 3.0),
-                // Club 2 Image with Name
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-
-                  children: [
-
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset(
-                        game.club2Picture,
-                        width: 30,
-                        height: 30,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const SizedBox(width: 12.0),
-                    Text(
-                      game.clubName2,
-                      style: const TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 3.0),
-                Text(
-                  '${game.dayOfMatch}  ${game.startTime}',
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                    fontFamily: 'Montserrat',
-                    color: Colors.white,
+                    ],
                   ),
-                ), // Enter Dugout Button with Spacer
-              ],
+                  const SizedBox(height: 8.0),
+                  // Club 2 Image with Name
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          game.club2Picture,
+                          width: 40, // Adjust width as needed
+                          height: 40, // Adjust height as needed
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(width: 12.0),
+                      Text(
+                        game.clubName2,
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    '${game.dayOfMatch}  ${game.startTime}',
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Montserrat',
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
             const Spacer(),
             ElevatedButton(
               onPressed: onEnterDugout,
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromRGBO(
-                    116, 229, 144, 1.0), // Button background color
+                backgroundColor: const Color.fromRGBO(116, 229, 144, 1.0), // Button background color
+                minimumSize: const Size(120, 60), // Set button height here
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(6.0), // Rounded corners
                 ),
               ),
-              child: const Text('Enter Dugout', style: TextStyle(fontFamily: 'Montserrat',
-                      color: Color(0xFF171A25)),),
+              child: const Text(
+                'Enter Dugout',
+              
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.0, // Adjust the spacing here
+                  fontFamily: 'Montserrat',
+                  color: Color(0xFF171A25),
+                ),
+              ),
             ),
           ],
         ),
